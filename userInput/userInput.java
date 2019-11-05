29fed08dc078e27f27132a11b9bd66f4fd45c738
Makavura <makavuramughanga@gmail.com>
2019-11-05 06:31:23 +0300

    ...

--- userInput/userInput.java
+++ userInput/userInput.java
@@ -10,6 +10,14 @@ public class userInput {
         System.out.println("Jina lako nani?");
         String name = input.next();
         System.out.println("Hujambo " + name);
+
+        System.out.println("Una miaka mingapi?");
+        int age = input.nextInt();
+        System.out.println("Uzani wako ni nini?");
+        double weight = input.nextDouble();
+
+        System.out.println("BMI Computation coming soon");
+
     }
 
 
